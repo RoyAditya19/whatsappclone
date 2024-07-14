e9308e0d86554dc16930fb528a0722ec685d116a
Adii <royaditya1981@gmail.com>
2024-07-14 17:31:37 +0530

    day 2 commit

--- whatsappclone/client/src/pages/_app.jsx
+++ whatsappclone/client/src/pages/_app.jsx
@@ -1,5 +1,17 @@
+import { StateProvider } from "@/context/StateContext";
+import reducer, { initialState } from "@/context/StateReducers";
 import "@/styles/globals.css";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return(
+  <StateProvider initialState={initialState} reducer={reducer}>
+  {/* using the following head tag we changed the title from react to whatsapp */}
+  <Head>
+    <title>Whatsapp</title>
+    <link rel="shortcut icon" href="/favicon.png" />
+  </Head>
+  <Component {...pageProps} />;
+  </StateProvider>
+  )
 }
