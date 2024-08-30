8389160cdc859a8b847522213748e97520f9bbf2
Adii <royaditya1981@gmail.com>
2024-08-30 17:44:13 +0530

    context api notes updated

--- whatsappclone/client/src/context/StateContext.jsx
+++ whatsappclone/client/src/context/StateContext.jsx
@@ -1,5 +1,8 @@
 import { createContext, useContext, useReducer } from "react"
 
+//working of context api: at first all the constants and variable necessary are stored and declared. now as usual createcontext is used, then a stateprovider has been used
+//which is accepting and passing the props from app.jsx file to statecontext provider. now as the statecontext has all the required context, a new usestateprovider is used
+//and exported. now using this usestateprovider we can use the required contexts at whatever depth of the components.
 export const StateContext = createContext()
 export const StateProvider = ({initialState,reducer,children})=>
 (
