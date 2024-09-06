b9c4d68181cd7153b1115c570e0b2db148d4679d
Adii <royaditya1981@gmail.com>
2024-09-06 11:47:27 +0530

    image selection process

--- whatsappclone/client/src/components/common/Avatar.jsx
+++ whatsappclone/client/src/components/common/Avatar.jsx
@@ -79,6 +79,21 @@ function Avatar({type,image,setImage}) {
       <Image src={image} alt="avatar" className="rounded-full" fill/>
     </div>
       )}
+      {
+          /*
+          When the user clicks on "Change Profile Photo" in the `Avatar` component, the `isContentMenuVisible` state is updated to `true`. 
+          This change triggers the rendering of the `ContextMenu` component, which displays a context menu with various options. The options are dynamically generated and passed as props to the `ContextMenu`. 
+          Each option in the menu is linked to a specific callback function. When the user selects an option from the context menu, the `handleClick` function within the `ContextMenu` component is executed. 
+          This function first stops the propagation of the click event to prevent it from affecting other elements. It then sets the `isContentMenuVisible` state to `false`, thereby hiding the context menu. 
+          After closing the menu, the function calls the callback associated with the selected option, which handles the specific action chosen by the user.
+          For instance, if the user selects the "Upload Photo" option, the callback function sets `setGrabphoto(true)`. This action updates the `grabPhoto` state in the `Avatar` component. 
+          The `useEffect` hook within the `Avatar` component is configured to monitor changes to the `grabPhoto` state. When it detects that `grabPhoto` has been set to `true`, it programmatically triggers a click event on a hidden file input element. 
+          This file input element is managed by the `PhotoPicker` component, which is responsible for rendering the file input field within the DOM. This hidden input allows the user to select an image file from their device.
+          When a user selects a file, the `onChange` event of the file input is triggered, invoking the `photoPickerChange` function. This function is responsible for processing the selected file. 
+          It uses the `FileReader` API to read the file's content and convert it into a base64-encoded data URL. The `FileReader` asynchronously reads the file as a data URL, 
+          which is a string representation of the file's binary data encoded in base64 format. Once the file is successfully read, the `photoPickerChange` function updates the `image` state in the `Avatar` component with this base64 data URL, allowing the `Avatar` component to display the newly selected image.
+          */
+      }
     { type === "xl" && (
     <div className="relative cursor-pointer z-0"
     onMouseEnter={()=>setHover(true)}
