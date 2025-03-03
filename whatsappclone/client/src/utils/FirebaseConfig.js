ec4f73fedc8c162369cdb807359567aa5121cc1e
Aditya Mishra <129823866+RoyAditya19@users.noreply.github.com>
2025-03-03 11:59:31 +0530

    Update FirebaseConfig.js

--- whatsappclone/client/src/utils/FirebaseConfig.js
+++ whatsappclone/client/src/utils/FirebaseConfig.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
 import {getAuth} from "firebase/auth";
 const firebaseConfig = {
-    apiKey: "AIzaSyCiySwtEDvjaYFloP7yqyNUVz_o-2n9iWI",
+    apiKey: "",
     authDomain: "whatsappclone-88dbe.firebaseapp.com",
     projectId: "whatsappclone-88dbe",
     storageBucket: "whatsappclone-88dbe.appspot.com",
